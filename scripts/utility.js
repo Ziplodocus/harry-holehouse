661cd3ed31ab85edf6a6bfa5cf35b42fe0beed24
Harry-Purple <haroldholehouse@gmail.com>
2021-05-04 19:47:07 +0100

    js based sideways nav

--- scripts/utility.js
+++ scripts/utility.js
@@ -1,23 +1,38 @@
-//Defines buttons and sections from the DOM
-const buttons = document.getElementsByClassName('nav-button');
-const sections = document.getElementsByClassName("content");
+//References to translate variables
+let previous = 1;
+let times = [0, 800, 1100, 1400];
+const targetPositions = {
+    home: '1',
+    profile: '2',
+    projects: '3',
+    contact: '4',
+};
 
-//Navigate function shows the section with id = button.value and hides the rest if neccessary
+//Navigate shows the content associated with nav button
 function navigate() {
-    for (let i = 0; i < sections.length; i++) {
-        let section = sections[i];
-        if (this.value === section.id && section.hasAttribute('hidden')) {
-            console.log(this.value + " shown!");
-            section.removeAttribute('hidden');
-        }
-        else if (!(this.value === section.id) && !section.hasAttribute('hidden')) {
-            console.log(section.id + " hidden");
-            section.setAttribute("hidden", "");
+    //gets button name value and compares with positions
+    //to receive the reference the the css variable
+    let targetSection = this.name.toString();
+    let targetPosition = '';
+    for (let section in targetPositions) {
+        if(section == targetSection) {
+            targetPosition = targetPositions[section];
+            break
         }
     }
+    //Determines which transition timing varibale to pick
+    let translateTime = times[Math.abs(previous - parseInt(targetPositions[targetSection]))].toString();
+    //Changes the translation and timing for consistency
+    $(".slider").css("--nav-time", `var(--translate-${translateTime})`);
+    $(".slider").css("--content-translate", `var(--translate-${targetPosition})`);
+    //Tracks the previous position of the slider
+    //so the timing can be extended for further
+    //translation
+    previous = parseInt(targetPosition);
 }
 
-//Adds event listeners to all the nav-buttons
-for (let i = 0; i < buttons.length; i++) {
-    buttons[i].addEventListener("click", navigate);
-};
+$(document).ready(() => {
+    //Adds event listeners to all the nav-buttons
+    const buttons = $('.nav-button');
+    buttons.on("click", navigate);
+})
